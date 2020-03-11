e5689e9fe1c979de5615fddca5b54d28784c8603
mikezalik <mike.zalik@gmail.com>
2020-03-11 14:30:42 -0700

    Adds MutableLiveData for countries, country load error and loading to viewmodel

--- app/src/main/java/com/mikezalik/countries/viewmodel/ListViewModel.kt
+++ app/src/main/java/com/mikezalik/countries/viewmodel/ListViewModel.kt
@@ -1,6 +1,20 @@
 package com.mikezalik.countries.viewmodel
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.mikezalik.countries.model.Country
 
 class ListViewModel: ViewModel() {
+
+    val countries = MutableLiveData<List<Country>>()
+    val countryLoadError = MutableLiveData<Boolean>()
+    val loading = MutableLiveData<Boolean>()
+
+    fun refresh() {
+        fetchCountries()
+    }
+
+    private fun fetchCountries() {
+
+    }
 }

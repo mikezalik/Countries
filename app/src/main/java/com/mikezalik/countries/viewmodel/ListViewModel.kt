bb6d3034c616f3faa9f2b3b2a12f5d42863c6f8c
mikezalik <mike.zalik@gmail.com>
2020-03-11 14:37:49 -0700

    Completes fetch countries using mock data

--- app/src/main/java/com/mikezalik/countries/viewmodel/ListViewModel.kt
+++ app/src/main/java/com/mikezalik/countries/viewmodel/ListViewModel.kt
@@ -15,6 +15,18 @@ class ListViewModel: ViewModel() {
     }
 
     private fun fetchCountries() {
+        val mockData = listOf(Country("CountryA"),
+            Country("CountryB"),
+            Country("CountryC"),
+            Country("CountryD"),
+            Country("CountryE"),
+            Country("CountryF"),
+            Country("CountryG"),
+            Country("CountryH")
+        )
 
+        countryLoadError.value = false
+        loading.value = false
+        countries.value = mockData
     }
 }

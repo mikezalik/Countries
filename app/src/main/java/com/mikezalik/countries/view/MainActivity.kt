aeaa5359f94cd70fa62d3da686c6bebe03a46763
mikezalik <mike.zalik@gmail.com>
2020-03-13 11:45:19 -0700

    Fixes errors with SwipeRefresh by switching to AndroidX and adds view model and countries adapter to main activity

--- app/src/main/java/com/mikezalik/countries/view/MainActivity.kt
+++ app/src/main/java/com/mikezalik/countries/view/MainActivity.kt
@@ -2,12 +2,27 @@ package com.mikezalik.countries.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.ViewModelProviders
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.mikezalik.countries.R
+import com.mikezalik.countries.viewmodel.ListViewModel
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var viewModel: ListViewModel
+    private val countriesAdapter = CountryListAdapter(arrayListOf())
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        viewModel = ViewModelProviders.of(this).get(ListViewModel::class.java)
+        viewModel.refresh()
+
+        countriesList.apply {
+            layoutManager = LinearLayoutManager(context)
+            adapter = countriesAdapter
+        }
     }
 }

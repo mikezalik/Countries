3d095bba73400a51640778dc1bbac15d08c6e695
mikezalik <mike.zalik@gmail.com>
2020-03-13 11:15:23 -0700

    Completes the country list adapter

--- app/src/main/java/com/mikezalik/countries/view/CountryListAdapter.kt
+++ app/src/main/java/com/mikezalik/countries/view/CountryListAdapter.kt
@@ -1,20 +1,33 @@
 package com.mikezalik.countries.view
 
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.mikezalik.countries.R
 import com.mikezalik.countries.model.Country
+import kotlinx.android.synthetic.main.item_country.view.*
 
 class CountryListAdapter(var countries:ArrayList<Country>): RecyclerView.Adapter<CountryListAdapter.CountryViewHolder>() {
+    fun updateCountries(newCountries: List<Country>) {
+        countries.clear()
+        countries.addAll(newCountries)
+        notifyDataSetChanged()
+    }
+
     class CountryViewHolder(view: View): RecyclerView.ViewHolder(view) {
+
+        private val countryName = view.name
+
         fun bind(country: Country) {
+            countryName.text = country.countryName
 
         }
     }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CountryViewHolder {
-        TODO("Not yet implemented")
-    }
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) = CountryViewHolder(
+        LayoutInflater.from(parent.context).inflate(R.layout.item_country, parent, false)
+    )
 
     override fun getItemCount() = countries.size
 

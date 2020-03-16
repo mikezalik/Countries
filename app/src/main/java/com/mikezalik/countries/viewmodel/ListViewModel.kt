4456164cedf188ea28a0b9fb64cbee18547624a9
mikezalik <mike.zalik@gmail.com>
2020-03-16 11:26:33 -0700

    Adds onSuccess and onError to fetchCountries

--- app/src/main/java/com/mikezalik/countries/viewmodel/ListViewModel.kt
+++ app/src/main/java/com/mikezalik/countries/viewmodel/ListViewModel.kt
@@ -2,9 +2,17 @@ package com.mikezalik.countries.viewmodel
 
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.mikezalik.countries.model.CountriesService
 import com.mikezalik.countries.model.Country
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.observers.DisposableSingleObserver
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
+import io.reactivex.schedulers.Schedulers
 
-class ListViewModel: ViewModel() {
+class ListViewModel : ViewModel() {
+
+    private val countriesService = CountriesService()
+    private val disposable = CompositeDisposable()
 
     val countries = MutableLiveData<List<Country>>()
     val countryLoadError = MutableLiveData<Boolean>()
@@ -15,6 +23,27 @@ class ListViewModel: ViewModel() {
     }
 
     private fun fetchCountries() {
+        loading.value = true
+        disposable.add(
+            countriesService.getCountries().subscribeOn(Schedulers.newThread())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribeWith(object : DisposableSingleObserver<List<Country>>() {
+                    override fun onSuccess(value: List<Country>?) {
+                        countries.value = value
+                        countryLoadError.value = false
+                        loading.value = false
+                    }
 
+                    override fun onError(e: Throwable?) {
+                        countryLoadError.value = true
+                        loading.value = false
+                    }
+                })
+        )
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        disposable.clear()
     }
 }

c37776cc00f867b44d623dff325d823a6cc6ad71
mikezalik <mike.zalik@gmail.com>
2020-03-17 11:17:30 -0700

    Adds success path unit test

--- app/src/test/java/com/mikezalik/countries/ListViewModelTest.kt
+++ app/src/test/java/com/mikezalik/countries/ListViewModelTest.kt
@@ -1,13 +1,25 @@
 package com.mikezalik.countries
 
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import com.mikezalik.countries.model.CountriesService
+import com.mikezalik.countries.model.Country
+import com.mikezalik.countries.viewmodel.ListViewModel
 import io.reactivex.Scheduler
+import io.reactivex.Single
 import io.reactivex.android.plugins.RxAndroidPlugins
 import io.reactivex.disposables.Disposable
 import io.reactivex.internal.schedulers.ExecutorScheduler
 import io.reactivex.plugins.RxJavaPlugins
+import junit.framework.Assert.assertEquals
+import org.junit.Assert
 import org.junit.Before
 import org.junit.Rule
+import org.junit.Test
+import org.mockito.InjectMocks
+import org.mockito.Mock
+import org.mockito.Mockito
+import org.mockito.Mockito.`when`
+import org.mockito.MockitoAnnotations
 import java.util.concurrent.Callable
 import java.util.concurrent.Executor
 import java.util.concurrent.TimeUnit
@@ -16,14 +28,41 @@ class ListViewModelTest {
     @get:Rule
     var rule = InstantTaskExecutorRule()
 
+    @Mock
+    lateinit var countriesService: CountriesService
+
+    @InjectMocks
+    lateinit var listViewModel: ListViewModel
+
+    private var testSingle: Single<List<Country>>? = null
+
+    @Before
+    fun setup() {
+        MockitoAnnotations.initMocks(this)
+    }
+
+    @Test
+    fun getCountriesSuccess() {
+        val country = Country("countryName", "capital", "url")
+        val countriesList = arrayListOf(country)
+
+        testSingle = Single.just(countriesList)
+
+        `when`(countriesService.getCountries()).thenReturn(testSingle)
+
+        listViewModel.refresh()
+
+        Assert.assertEquals(1, listViewModel.countries.value?.size)
+        Assert.assertEquals(false, listViewModel.countryLoadError.value)
+        Assert.assertEquals(false, listViewModel.loading.value)
+    }
+
     @Before
     fun setUpRxSchedulers() {
         val immediate = object: Scheduler() {
             override fun scheduleDirect(run: Runnable?, delay: Long, unit: TimeUnit?): Disposable {
                 return super.scheduleDirect(run, 0, unit)
             }
-
-
 
             override fun createWorker(): Worker {
                 return ExecutorScheduler.ExecutorWorker(Executor { it.run ()})
